e38976e8869a30e34e932ed97ef086ae13723625
David Duvenaud <dkd23@cam.ac.uk>
2012-11-08 17:48:52 +0000

    Minor fix to qsub code

--- source/runstuff.py
+++ source/runstuff.py
@@ -17,7 +17,7 @@ def call_cluster(k=3, max_depth=2):
                ]
     
     #fear_string = '''. /usr/local/grid/divf2/common/settings.sh; qsub -l lr=0 -o "/home/mlg/dkd23/large_results/fear_logs_gpss/gpss_run_log_%(dataset)s.txt" -e "/home/mlg/dkd23/large_results/fear_logs_gpss/gpss_error_log_%(dataset)s.txt"  /home/mlg/dkd23/git/gp-structure-search/source/run_python.sh /home/mlg/dkd23/git/gp-structure-search/%(dataset)s /home/mlg/dkd23/git/gp-structure-search/%(results_filename)s %(depth)s %(k)s'''
-    fear_string = '''. /usr/local/grid/divf2/common/settings.sh; qsub -l lr=0 /home/mlg/dkd23/git/gp-structure-search/source/run_python.sh /home/mlg/dkd23/git/gp-structure-search/%(dataset)s /home/mlg/dkd23/git/gp-structure-search/%(results_filename)s %(depth)s %(k)s'''
+    fear_string = '''. /usr/local/grid/divf2/common/settings.sh; qsub -l lr=0 /home/mlg/dkd23/git/gpss_stable/source/run_python.sh /home/mlg/dkd23/git/gpss_stable/%(dataset)s /home/mlg/dkd23/git/gpss_stable/%(results_filename)s %(depth)s %(k)s'''
     
     for data_file in datasets:
         results_filename = 'experiment_output_%s_depth=%d_k=%d' % ( data_file, max_depth, k )
--- source/sandpit.py
+++ source/sandpit.py
@@ -339,22 +339,7 @@ def full_gef_load_experiment(zone=1, max_depth=5, verbose=True):
             
         seed_kernels = [r[0] for r in sorted(new_results, key=lambda p: p[active_key])[0:k]]
         
-        
-def call_cluster(k=3, max_depth=2):
-    '''Run experiments on all data sets on the cluster'''
-    datasets = ['data/mauna2003.mat', \
-                'data/abalone_500.mat', \
-                'data/bach_synth_r_200.mat', \
-                'data/r_concrete_500.mat', \
-                'data/housing.mat' \
-               ]
-    
-    fear_string = '''. /usr/local/grid/divf2/common/settings.sh; qsub -l lr=0 -o "/home/mlg/dkd23/large_results/fear_logs_gpss/gpss_run_log_%(dataset)s.txt" -e "/home/mlg/dkd23/large_results/fear_logs_gpss/gpss_error_log_%(dataset)s.txt" /home/mlg/dkd23/git/gp-structure-search/python structure_seach.py %(dataset)s %(results_filename)s %(depth)s %(k)s'''
-    
-    for data_file in datasets:
-        results_filename = 'experiment_output_%s_depth=%d_k=%d' % ( data_file, max_depth, k )
-        command_str = fear_string % {'dataset' : data_file, 'results_filename' : results_filename, 'depth' : max_depth, 'k' : k }
-        print command_str
+
         #os.system(command_str)
 
 if __name__ == '__main__':
