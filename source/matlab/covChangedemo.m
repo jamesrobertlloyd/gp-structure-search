3d439297f9eb521457b1730133581ac377d96ea7
David Duvenaud <dkd23@cam.ac.uk>
2013-02-10 15:34:53 +0000

    Reparamaterized changepoint kernel and checked derivatives

---
+++ source/matlab/covChangedemo.m
@@ -0,0 +1,34 @@
+function covChangedemo()
+% A simple script to demonstrate the changepoint covariance.
+%
+% David Duvenaud
+% Feb 2013
+
+addpath(genpath('/scratch/Dropbox/code/gpml/'))
+
+n = 100;
+D = 1;
+x = linspace(-5, 5, n)';
+
+
+covfunc = {'covProd',{'covRQiso','covChange'}};
+num_hypers = eval(feval(covfunc{:}));
+hypers = randn(1, num_hypers)
+
+K = feval( covfunc{:}, hypers, x, x);
+
+figure(1); clf; imagesc(K)
+
+L = chol(K + eye(n).*max(K(:)).*1e-6);
+
+y = L*randn(n,1);
+figure(2); clf; plot(x,y)
+
+end
+
+function ng = numerical_grad( f, x, i, delta )
+% Takes a function of x, and computes gradient w.r.t. dimension i.
+    x2 = x;
+    x2(i) = x2(i) + delta;
+    ng = (f(x2) - f(x))./delta;
+end
--- source/matlab/custom-cov/covChange.m
+++ source/matlab/custom-cov/covChange.m
@@ -1,64 +1,58 @@
 function K = covChange(hyp, x, z, i)
 
-% Changepoint covariance function with scale and input space shift. The
-% covariance function is parameterized as:
+% 1D changepoint covariance function with inverse smoothness alpha,
+% and input space shift. The covariance function is parameterized as:
 %
-% k(x,x') = sf2 * sigmoid(sum((x - shift)/ell))*sigmoid(sum((x - shift)/ell))
+% k(x,x') = sigmoid(sum((x - shift)*alpha))*sigmoid(sum((x - shift)*alpha))
+%
+% Sigmoid ranges from 0 to 1.
 %
 % The hyperparameters are:
 %
-% hyp = [ log(sf2)
-%         log(ell)
-%         shifts ]
+% hyp = [ alpha
+%         shift ]
 %
 % David Duvenaud
 % February 2013
 
 
-if nargin<2, K = '1 + 2*D'; return; end            % report number of parameters
+if nargin<2, K = '2'; return; end                  % report number of parameters
 if nargin<3, z = []; end                                   % make sure, z exists
 xeqz = numel(z)==0;
 dg = strcmp(z,'diag') && numel(z)>0;                            % determine mode
 
 [n,D] = size(x);
-[nz,Dz] = size(z);
-sf2 = exp(hyp(1));
-ell = exp(hyp(2:D+1));
-shifts = hyp((D+2):(2*D+1));
+alpha = hyp(1);
+shift = hyp(2);
 
-x = sum((x-repmat(shifts',n,1))*diag(1./ell), 2);        % x is now one-dimensional
-if nz > 0
-    z = sum((z-repmat(shifts',nz,1))*diag(1./ell), 2);        % z is now one-dimensional
-end
+sumx = sum(x-shift,2);
+x = sumx*alpha;        % x is now one-dimensional
+if xeqz                % This is the slow way.
+    z = x;
+else
+    sumz = sum(z-shift,2);
+    z = sumz*alpha;
+end 
 
 sx = 1 ./ (1 + exp(-x));   % sigmoid
 sz = 1 ./ (1 + exp(-z));   % sigmoid
 
 if dg                                                               % vector kxx
-  K = sx.*sx;
+    K = sx.*sx;
 else
-  if xeqz                                                 % symmetric matrix Kxx
-    K = x*x';
-  else                                                   % cross covariances Kxz
-    K = x*z';
-  end
+    K = sx*sz';
 end
 
-if nargin<4                                                        % covariances
-  K = sf2*K;
-else                                                               % derivatives
-  if i==1                                                          % magnitude
-    K = sf2*K;
-  elseif i<=D+1                                                    % ell
-    i = i - D;
-    if dg                    % derivative of vector kxx w.r.t. lengthscales
-      K = sf2 * -2*sx*K*sx'./ell(i);
+if nargin>3                                                        % covariances
+  if i==1                                                          % alpha
+    if dg                    % derivative of vector kxx w.r.t. alpha
+      K = diag(K .* bsxfun(@plus,(1 - sx).*sumx, (1 - sz').*sumz'));
     else
-      K = sf2 * -(sx.*(1-sx))*(sx.*(1-sx))'./(ell(i)^2);
+      K = K .* bsxfun(@plus,(1 - sx).*sumx, (1 - sz').*sumz');
     end
-  elseif i<=2*D+1                                             % shifts
-    i = i - D*2;
-    K = sf2 * (sx.*(1-sx))*(sx.*(1-sx))'./(ell(i));
+  elseif i==2                                          % shifts
+      % Todo: replace D with sum over shifts
+    K = -D*alpha * K .* ( 2 - bsxfun(@plus,sx, sz') );
   else
     error('Unknown hyperparameter')
   end
