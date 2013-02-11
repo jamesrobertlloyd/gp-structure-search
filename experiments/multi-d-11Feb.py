532f5a50b9ad69f5f3bb7bac6395d88d9b5c1832
James Robert Lloyd <jrl44@cam.ac.uk>
2013-02-11 12:02:07 +0000

    Corrected some experiment specifications

--- experiments/multi-d-11Feb.py
+++ experiments/multi-d-11Feb.py
@@ -1,20 +1,20 @@
 # Runs all 1d datasets.
 
-Experiment(description='Run all 1D datasets',
-           data_dir='../data/1d_extrap_folds/',
-           max_depth=8, 
+Experiment(description='Run all multi D datasets',
+           data_dir='../data/kfold_data/',
+           max_depth=12, 
            random_order=False,
            k=1,
            debug=False, 
            local_computation=False, 
-           n_rand=9,
+           n_rand=2,
            sd=4, 
            max_jobs=500, 
            verbose=False,
            make_predictions=True,
            skip_complete=True,
-           results_dir='../results/9-Feb 1d learning curves/',
-           iters=200)
+           results_dir='../results/11-Feb/',
+           iters=100)
            
 
            
