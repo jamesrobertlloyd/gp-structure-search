233048366e86d16d43b6c60cc7b1b1137a3ba8bb
James Robert Lloyd <jrl44@cam.ac.uk>
2013-02-15 10:55:53 +0000

    New extrap results - processing...

---
+++ experiments/1d-ex-roger-15Feb-random.py
@@ -0,0 +1,20 @@
+# Runs all 1d extrapolations.
+
+Experiment(description='Run all 1D extrapolations',
+           data_dir='../data/1d_extrap_roger_folds/',
+           max_depth=8, 
+           random_order=True,
+           k=1,
+           debug=False, 
+           local_computation=False, 
+           n_rand=3,
+           sd=4, 
+           max_jobs=500, 
+           verbose=False,
+           make_predictions=True,
+           skip_complete=True,
+           results_dir='../results/15-Feb-1d-extrap-roger/',
+           iters=100)
+           
+
+           
