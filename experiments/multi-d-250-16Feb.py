23dc522c8f3d59b175312d7d2125a7e8ee9462a8
James Robert Lloyd <jrl44@cam.ac.uk>
2013-02-21 11:57:54 +0000

    Some ongoing experiment output added

---
+++ experiments/multi-d-250-16Feb.py
@@ -0,0 +1,20 @@
+# Runs all 1d datasets.
+
+Experiment(description='Run all multi D datasets',
+           data_dir='../data/kfold_data/',
+           max_depth=12, 
+           random_order=False,
+           k=1,
+           debug=False, 
+           local_computation=False, 
+           n_rand=2,
+           sd=4, 
+           max_jobs=250, 
+           verbose=False,
+           make_predictions=True,
+           skip_complete=True,
+           results_dir='../results/16-Feb/',
+           iters=250)
+           
+
+           
