0a2c3379b1860516aef689f0ffb73ec8aa4df6f8
James Lloyd <james.robert.lloyd@gmail.com>
2013-02-28 18:23:02 +0000

    New synthetic experiments with zero mean and const

---
+++ experiments/synthetic-const-28-Feb-SNR-tenth.py
@@ -0,0 +1,22 @@
+# Runs all synthetic datasets
+
+Experiment(description='Run all synthetic datasets',
+           data_dir='../data/synthetic-SNR-tenth/',
+           max_depth=8, 
+           random_order=False,
+           k=1,
+           debug=False, 
+           local_computation=False, 
+           n_rand=19,
+           sd=4, 
+           max_jobs=600, 
+           verbose=False,
+           make_predictions=False,
+           skip_complete=True,
+           results_dir='../results/28-Feb-synthetic-const-SNR-tenth/',
+           iters=100,
+           base_kernels='SE,RQ,Per,Lin,Const',
+           zero_mean=True)
+           
+
+           
