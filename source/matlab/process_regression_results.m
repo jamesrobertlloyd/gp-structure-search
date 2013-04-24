9c05caeb0a9e3dd05966effad332259621818550
James Robert Lloyd <jrl44@cam.ac.uk>
2013-04-24 16:37:08 +0100

    Starting to update tex in response to reviews

--- source/matlab/process_regression_results.m
+++ source/matlab/process_regression_results.m
@@ -6,8 +6,8 @@ experiments = {};
 experiments{1} = 'bach_synth_r_200';
 experiments{2} = 'r_concrete_500';
 experiments{3} = 'r_pumadyn512';
-%experiments{4} = 'r_servo';
-experiments{4} = 'r_housing';
+experiments{4} = 'r_servo';
+experiments{5} = 'r_housing';
 
 %folder = '../saved_results/9-Jan/';
 %folder = '../saved_results/18-Jan/';
