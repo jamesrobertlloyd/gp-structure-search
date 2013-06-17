c9a64a117894f64b0d2bdb02b1a4d29c3c59acf0
jamesrobertlloyd <james.robert.lloyd@gmail.com>
2013-06-17 18:29:19 -0300

    Made comment more precise

--- source/cblparallel/config_example.py
+++ source/cblparallel/config_example.py
@@ -1,4 +1,4 @@
 LOCAL_TEMP_PATH            = '/Users/JamesLloyd/temp' # A directory where temporary files can be written safely
 LOCAL_MATLAB               = '/Applications/MATLAB_R2011b.app/bin/matlab' # Path to matlab - on some machines this will just be 'matlab'
-LOCATION                   = 'local' # A vestigial feature
+LOCATION                   = 'local' # A vestigial feature - keep as is
 LOCAL_GPML_PATH            = '/Users/JamesLloyd/Desktop/gp-structure-search-master/source/gpml' # Location of GPML
