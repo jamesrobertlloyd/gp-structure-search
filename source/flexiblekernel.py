73f2948e4eae6d020c9b4397950bcc42a117d07d
James Robert Lloyd <jrl44@cam.ac.uk>
2013-09-10 15:39:06 +0100

    Removed laplace - not stable

--- source/flexiblekernel.py
+++ source/flexiblekernel.py
@@ -1888,7 +1888,8 @@ class ScoredKernel:
     def from_matlab_output(output, kernel_family, ndata):
         '''Computes Laplace marginal lik approx and BIC - returns scored Kernel'''
         #laplace_nle, problems = psd_matrices.laplace_approx_stable(output.nll, output.kernel_hypers, output.hessian)
-        laplace_nle = psd_matrices.laplace_approx_no_prior(output.nll, np.concatenate((output.kernel_hypers, output.noise_hyp)), output.hessian)
+        #laplace_nle = psd_matrices.laplace_approx_no_prior(output.nll, np.concatenate((output.kernel_hypers, output.noise_hyp)), output.hessian)
+        laplace_nle = np.nan
         k_opt = kernel_family.from_param_vector(output.kernel_hypers)
         BIC = 2 * output.nll + k_opt.effective_params() * np.log(ndata)
         return ScoredKernel(k_opt, output.nll, laplace_nle, BIC, output.noise_hyp)	
